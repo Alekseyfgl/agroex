90624981aed2ba4b13d332211619277f36a85e55
Denis <denkazarin@icloud.com>
2022-05-17 18:47:49 +0300

    add connectionToHeroku

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -11,18 +11,24 @@ export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
   useFactory: async (): Promise<TypeOrmModuleOptions> => {
     return {
       type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USERNAME,
-      database: process.env.POSTGRES_DB,
-      password: process.env.POSTGRES_PASSWORD,
+      url: 'postgres://mhrpqbhtkrwpot:52af8c6c7ae1cc34bf031fff523146c871ec021fdf2cdd2c9bbd727b86fe13d3@ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/denneq89n3dfuq',
+
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+          charset: 'utf8mb4_unicode_ci',
+        },
+      },
+      // url: 'postgres://postgres:root@localhost:5432/categories',
+      // host: process.env.POSTGRES_HOST,
+      // port: Number(process.env.POSTGRES_PORT),
+      // username: 'postgres',
+      // database: process.env.POSTGRES_DB,
+      // password: 'ec2-52-30-67-143.eu-west-1.compute.amazonaws.com',
       entities: [__dirname + '/../**/*.entity.{js,ts}'],
       migrations: [__dirname + '/../database/migrations/*{.ts,.js}'],
       cli: {
         migrationsDir: __dirname + '/../database/migrations',
-      },
-      extra: {
-        charset: 'utf8mb4_unicode_ci',
       },
       synchronize: false,
       logging: true,
@@ -32,19 +38,19 @@ export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  port: Number(process.env.POSTGRES_PORT),
-  username: process.env.POSTGRES_USERNAME,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
+  url: 'postgres://mhrpqbhtkrwpot:52af8c6c7ae1cc34bf031fff523146c871ec021fdf2cdd2c9bbd727b86fe13d3@ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/denneq89n3dfuq',
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+      charset: 'utf8mb4_unicode_ci',
+    },
+  },
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   migrations: [__dirname + '/../database/migrations/*{.ts,.js}'],
   cli: {
     migrationsDir: __dirname + '/../database/migrations',
   },
-  extra: {
-    charset: 'utf8mb4_unicode_ci',
-  },
   synchronize: false,
   logging: true,
 };
+
---
+++ src/database/migrations/1652795778549-TestMigrationToReleaseOnHeroku.ts
@@ -0,0 +1,16 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class TestMigrationToReleaseOnHeroku1652795778549 implements MigrationInterface {
+    name = 'TestMigrationToReleaseOnHeroku1652795778549';
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+          `INSERT INTO "categories" VALUES (5,'Cereals')`,
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "categories"`);
+    }
+
+}
