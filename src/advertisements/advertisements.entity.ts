cd7a6e6b5b856d686c69e8ea7ee85eab5a081068
Aleksey.fgl@gmail.com <Aleksey.fgl@gmail.com>
2022-05-30 22:17:36 +0300

    add date in table

--- src/advertisements/advertisements.entity.ts
+++ src/advertisements/advertisements.entity.ts
@@ -1,11 +1,11 @@
-import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+import {BeforeUpdate, Column, Entity, PrimaryGeneratedColumn} from "typeorm";
 
 @Entity({name: 'advertisements'})
 export class AdvertisementsEntity {
     @PrimaryGeneratedColumn('increment')
     id: number;
 
-    @Column("varchar", { length: 200 })
+    @Column("varchar", {length: 200})
     title: string;
 
     @Column()
@@ -14,7 +14,7 @@ export class AdvertisementsEntity {
     @Column('decimal', {precision: 18, scale: 2})
     price: number;
 
-    @Column("varchar", { length: 3 })
+    @Column("varchar", {length: 3})
     currency: string;
 
     @Column()
@@ -23,6 +23,17 @@ export class AdvertisementsEntity {
     @Column('decimal', {precision: 18, scale: 2})
     weight: number;
 
-    @Column("varchar", { length: 5 })
+    @Column("varchar", {length: 5})
     unit: string;
+
+    @Column({type: 'timestamp', default: () => 'CURRENT_TIMESTAMP'})
+    createAt: Date
+
+    @Column({type: 'timestamp', default: () => 'CURRENT_TIMESTAMP'})
+    updatedAt: Date
+
+    @BeforeUpdate()//чтобы автоматически обновлять updatedAt
+    updateTimestamp() {
+        this.updatedAt = new Date();
+    }
 }
