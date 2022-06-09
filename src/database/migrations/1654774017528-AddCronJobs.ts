d3cb1febeb52520b662804e8c024518fb1301c8a
Denis <denkazarin@icloud.com>
2022-06-09 17:38:14 +0300

    change migration

--- src/database/migrations/1654774017528-AddCronJobs.ts
+++ src/database/migrations/1654774017528-AddCronJobs.ts
@@ -6,7 +6,7 @@ export class AddCronJobs1654774017528 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "cronJobs" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "jobType" character varying NOT NULL, "date" TIMESTAMP NOT NULL DEFAULT now(), "targetId" integer NOT NULL, CONSTRAINT "PK_8d6395d577c72fdb5fc97a9d956" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "advertisements" ADD "isActive" boolean NOT NULL DEFAULT false`);
-        await queryRunner.query(`ALTER TABLE "advertisements" ADD "expireAdvert" TIMESTAMP DEFAULT now() + interval '5 minute'`);
+        await queryRunner.query(`ALTER TABLE "advertisements" ADD "expireAdvert" TIMESTAMP DEFAULT now() + interval '3 days'`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
