e440db82e55462bc8242cc6f063a0dd3f3cf1de5
Aleksey.fgl@gmail.com <Aleksey.fgl@gmail.com>
2022-06-09 21:35:10 +0300

    correct betDto

--- src/bets/dto/createBet.dto.ts
+++ src/bets/dto/createBet.dto.ts
@@ -5,7 +5,7 @@ import {Transform} from "class-transformer";
 export class CreateBetDto {
     @Transform(({value}) => Number(value), {toClassOnly: true}) //преобразует в number
     @IsNumber()
-    @Min(0)
+    @Min(1)
     @Max(10000000000)
     betValue: number
 }
