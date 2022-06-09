fbdbd918454561b648893c0dcf22318aa64b561b
Aleksey.fgl@gmail.com <Aleksey.fgl@gmail.com>
2022-06-09 13:24:31 +0300

    correct mistakes

--- src/bets/bet.repository.ts
+++ src/bets/bet.repository.ts
@@ -3,21 +3,17 @@ import {UserBetEntity} from "./user-bet.entity";
 import {AdvertisementsEntity} from "../advertisements/advertisements.entity";
 import {UserEntity} from "../user/user.entity";
 import {CreateBetDto} from "./dto/createBet.dto";
-import {OptionalSQL} from "../interfacesAndTypes/optional.interface";
 
 
 @EntityRepository(UserBetEntity)
 export class BetRepository extends AbstractRepository<UserBetEntity> {
     async createBet(advert: AdvertisementsEntity, user: UserEntity, betObj: CreateBetDto): Promise<void> {
 
-        const betData: UserBetEntity = new UserBetEntity();
-        const newBet = Object.assign(betData, {
+        await this.repository.save({
             user_id: user.id,
             advertisement_id: advert.id,
             betValue: +betObj.betValue
         })
-
-        await this.repository.save(newBet)
         await this.changePreviousBet(advert.id)
     }
 
@@ -29,7 +25,7 @@ export class BetRepository extends AbstractRepository<UserBetEntity> {
                         limit 1 offset 1`
 
         try {
-            const penultimateBet: OptionalSQL<betAndAdvertInterface[]> = await this.repository.query(sql)
+            const penultimateBet: betAndAdvertInterface[] = await this.repository.query(sql)
 
             if (penultimateBet[0].isActive && penultimateBet.length) {
                 await this.repository.update({id: penultimateBet[0].id}, {isActive: false})
--- src/interfacesAndTypes/optional.interface.ts
+++ src/interfacesAndTypes/optional.interface.ts
@@ -1,3 +1,2 @@
 export type Optional<T> = T | null;
 export type PromiseOptional<T> = Promise<T | null>;
-export type OptionalSQL<T> = T | [];
