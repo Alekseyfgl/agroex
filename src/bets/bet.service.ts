c42abc3d06df27d4a746e08cc2c825934da9e64f
Aleksey.fgl@gmail.com <Aleksey.fgl@gmail.com>
2022-06-09 14:46:21 +0300

    add check for bets

--- src/bets/bet.service.ts
+++ src/bets/bet.service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@nestjs/common';
+import {HttpException, HttpStatus, Injectable} from '@nestjs/common';
 import {CreateBetDto} from './dto/createBet.dto';
 import {UserEntity} from "../user/user.entity";
 import {BetRepository} from "./bet.repository";
@@ -19,6 +19,42 @@ export class BetService {
         const advert: AdvertisementsEntity = await this.advertisementsService.getAdvertisementBySlug(currentSlug);
         const user: UserEntity = await this.userService.getUserById(currentUser);
 
-        await this.betRepository.createBet(advert, user, bet)
+
+        const AdvertisementWithLastBet: betAndAdvertInterface[] = await this.betRepository.getAdvertisementWithLastBet(advert.id);
+
+
+        const priceSeller: number = +advert.price;
+        const lastBet: number = +AdvertisementWithLastBet[0].betValue;
+        const currentBet: number = bet.betValue;
+
+
+        if (currentBet > priceSeller) {
+            throw new HttpException(
+                {
+                    status: HttpStatus.NOT_FOUND,
+                    message: ['Your price is higher than the seller'],
+                },
+                HttpStatus.NOT_FOUND,
+            );
+        }
+
+
+        if (!lastBet && priceSeller > currentBet) {
+
+            await this.betRepository.createBet(advert, user, bet);
+
+        } else if (lastBet < currentBet && priceSeller > currentBet) {
+
+            await this.betRepository.createBet(advert, user, bet);
+
+        } else {
+            throw new HttpException(
+                {
+                    status: HttpStatus.NOT_FOUND,
+                    message: ['Refresh the page, bets have already changed'],
+                },
+                HttpStatus.NOT_FOUND,
+            );
+        }
     }
 }
