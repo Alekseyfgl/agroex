aceddc00ed84ce7125d8195fb730299ca6d02cc3
Aleksey.fgl@gmail.com <Aleksey.fgl@gmail.com>
2022-06-15 19:22:16 +0300

    delete dto from orders

--- src/orders/dto/createOrder.dto.ts
+++
@@ -1,9 +0,0 @@
-import {Transform} from "class-transformer";
-import {IsNumber} from "class-validator";
-
-
-// export class CreateOrderDto {
-//     @Transform(({ value }) => Number(value), { toClassOnly: true }) //преобразует в number
-//     @IsNumber()
-//     acceptPrice: number
-// }
