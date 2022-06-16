8d98d83a3e38f49f59c30fb48a62ed9549b836c0
Aleksey.fgl@gmail.com <Aleksey.fgl@gmail.com>
2022-06-16 20:04:09 +0300

    add sort for my orders

--- src/orders/orders.repository.ts
+++ src/orders/orders.repository.ts
@@ -25,7 +25,8 @@ export class OrdersRepository extends AbstractRepository<OrdersEntity> {
                     LEFT JOIN users AS u ON u.id=adv."authorId"
                     LEFT JOIN "userBets" AS ub ON adv.id=ub.advertisement_id
                     INNER JOIN orders AS o ON ub.id = o.bet_id
-                    WHERE adv."authorId" = ${currentUserId} OR ub.user_id = ${currentUserId}`,
+                    WHERE adv."authorId" = ${currentUserId} OR ub.user_id = ${currentUserId}
+                    ORDER BY adv."createAt" DESC`,
     );
 
 
