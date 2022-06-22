4029693bc71297d6ae1599191b20dfa9ea1efd83
Aleksey.fgl@gmail.com <Aleksey.fgl@gmail.com>
2022-06-22 12:09:00 +0300

    correct func buyNow and change type accepted bets

--- src/bets/dto/createBet.dto.ts
+++ src/bets/dto/createBet.dto.ts
@@ -1,9 +1,9 @@
-import { IsNumber, Max, Min } from 'class-validator';
+import { IsInt, Max, Min } from 'class-validator';
 import { Transform } from 'class-transformer';
 
 export class CreateBetDto {
   @Transform(({ value }) => Number(value), { toClassOnly: true }) //преобразует в number
-  @IsNumber()
+  @IsInt()
   @Min(1)
   @Max(10000000000)
   betValue: number;
