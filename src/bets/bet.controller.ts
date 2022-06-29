8711fe7c48fe1e7cf18de8a954f0bde31cce6d8d
Aleksey.fgl@gmail.com <Aleksey.fgl@gmail.com>
2022-06-29 10:24:04 +0300

    add swagger for bets

--- src/bets/bet.controller.ts
+++ src/bets/bet.controller.ts
@@ -13,13 +13,16 @@ import { AuthGuard } from '../auth/guards/auth.guard';
 import { User } from '../user/decorators/user.decarator';
 import { UserEntity } from '../user/user.entity';
 import { UserBetEntity } from './user-bet.entity';
-import {ApiTags} from "@nestjs/swagger";
+import {ApiBody, ApiOperation, ApiResponse, ApiTags} from "@nestjs/swagger";
+import {GetAllAdsSwagger} from "../../swagger/AdsSwagger";
 
 @ApiTags('bets')
 @Controller()
 export class BetController {
   constructor(private readonly betService: BetService) {}
 
+  @ApiOperation({summary: 'Set bet as register user'})
+@ApiBody({type: CreateBetDto})
   @Post(':slug/bet')
   @UseGuards(AuthGuard)
   @UsePipes(new ValidationPipe())
--- src/bets/dto/createBet.dto.ts
+++ src/bets/dto/createBet.dto.ts
@@ -1,7 +1,9 @@
 import { IsInt, Max, Min } from 'class-validator';
 import { Transform } from 'class-transformer';
+import {ApiProperty} from "@nestjs/swagger";
 
 export class CreateBetDto {
+  @ApiProperty({example: 355})
   @Transform(({ value }) => Number(value), { toClassOnly: true }) //преобразует в number
   @IsInt()
   @Min(1)
